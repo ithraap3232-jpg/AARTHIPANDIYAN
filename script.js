675058d45a9a9855b4c8f36e284db128525abc87
ithraap3232-jpg <ithraap3232@gmail.com>
2026-06-23 07:27:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,99 @@
+// Welcome Message
+window.addEventListener("load", () => {
+    console.log("Welcome to Aarthi's Portfolio!");
+});
+
+// Smooth Scroll Effect
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function (e) {
+        e.preventDefault();
+
+        const target = document.querySelector(this.getAttribute("href"));
+
+        if (target) {
+            target.scrollIntoView({
+                behavior: "smooth"
+            });
+        }
+    });
+});
+
+// Active Navigation Highlight
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll("nav ul li a");
+
+window.addEventListener("scroll", () => {
+    let current = "";
+
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop - 100;
+        const sectionHeight = section.clientHeight;
+
+        if (pageYOffset >= sectionTop) {
+            current = section.getAttribute("id");
+        }
+    });
+
+    navLinks.forEach(link => {
+        link.classList.remove("active");
+
+        if (link.getAttribute("href") === `#${current}`) {
+            link.classList.add("active");
+        }
+    });
+});
+
+// Scroll Animation
+const revealElements = document.querySelectorAll("section");
+
+function revealOnScroll() {
+    revealElements.forEach(element => {
+        const windowHeight = window.innerHeight;
+        const elementTop = element.getBoundingClientRect().top;
+
+        if (elementTop < windowHeight - 100) {
+            element.classList.add("show");
+        }
+    });
+}
+
+window.addEventListener("scroll", revealOnScroll);
+revealOnScroll();
+
+// Back to Top Button
+const topButton = document.createElement("button");
+
+topButton.innerText = "↑";
+topButton.id = "topBtn";
+
+document.body.appendChild(topButton);
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 300) {
+        topButton.style.display = "block";
+    } else {
+        topButton.style.display = "none";
+    }
+});
+
+topButton.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
+let lastScrollTop = 0;
+const navbar = document.querySelector("nav");
+
+window.addEventListener("scroll", () => {
+    let currentScroll = window.pageYOffset;
+
+    if(currentScroll > lastScrollTop){
+        navbar.style.top = "-120px";
+    }
+    else{
+        navbar.style.top = "20px";
+    }
+
+    lastScrollTop = currentScroll;
+});
